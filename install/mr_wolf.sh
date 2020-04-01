74f3933c61a10e8afe6357793c591d42b65671b1
Mariano <marianosciacco@gmail.com>
2020-04-01 20:23:00 +0200

    Update mr_wolf.sh

--- install/mr_wolf.sh
+++ install/mr_wolf.sh
@@ -8,11 +8,11 @@ then
 	rm -rf thirema/
 fi
 mkdir thirema
-git clone https://github.com/RedRoundRobin/swe-kafka-db thirema/
-git clone https://github.com/RedRoundRobin/swe-gateway thirema/
-git clone https://github.com/RedRoundRobin/swe-api thirema/
-git clone https://github.com/RedRoundRobin/swe-telegram thirema/
-git clone https://github.com/RedRoundRobin/swe-webapp thirema/
+git clone https://github.com/RedRoundRobin/swe-kafka-db thirema/swe-kafka-db
+git clone https://github.com/RedRoundRobin/swe-gateway thirema/swe-gateway
+git clone https://github.com/RedRoundRobin/swe-api thirema/swe-api
+git clone https://github.com/RedRoundRobin/swe-telegram thirema/swe-telegram
+git clone https://github.com/RedRoundRobin/swe-webapp thirema/swe-webapp
 echo "> Have you ever given a foot massage? Stay calm and relax ..."
 cp docker-compose.thirema.yml thirema/
 docker-compose up --build -d \

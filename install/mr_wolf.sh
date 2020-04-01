7b79401c8512c77e62b9d389cd3e0551c80e11d4
Mariano <marianosciacco@gmail.com>
2020-04-01 20:32:31 +0200

    Update mr_wolf.sh

--- install/mr_wolf.sh
+++ install/mr_wolf.sh
@@ -4,8 +4,8 @@ if [ -d "thirema" ]
 then
 	docker-compose down -v \
 	-f thirema/docker-compose.thirema.yml \
-	-f thirema/swe-kafka-db/kafka/docker-compose.thirema.yml
-	rm -rf thirema/
+	-f thirema/swe-kafka-db/kafka/docker-compose.yml
+	rm -rf ./thirema/
 fi
 mkdir thirema
 git clone https://github.com/RedRoundRobin/swe-kafka-db thirema/swe-kafka-db
@@ -14,8 +14,8 @@ git clone https://github.com/RedRoundRobin/swe-api thirema/swe-api
 git clone https://github.com/RedRoundRobin/swe-telegram thirema/swe-telegram
 git clone https://github.com/RedRoundRobin/swe-webapp thirema/swe-webapp
 echo "> Have you ever given a foot massage? Stay calm and relax ..."
-cp docker-compose.thirema.yml thirema/
+cp ./docker-compose.thirema.yml thirema/
 docker-compose up --build -d \
 	-f thirema/docker-compose.thirema.yml \
-	-f thirema/swe-kafka-db/kafka/docker-compose.thirema.yml
+	-f thirema/swe-kafka-db/kafka/docker-compose.yml
 echo "> Mr. wolf solved our problems."

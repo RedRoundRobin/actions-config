26b234400ad0d558002208ab4802b8457422dd27
Mariano <marianosciacco@gmail.com>
2020-04-15 17:41:13 +0200

    Aggiornamento mister wolf

--- install/mr_wolf.sh
+++ install/mr_wolf.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
-echo "> You read the bible, Brett? Let's see..."
+
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo "============================================="
+echo "${ORANGE}[Mr. Wolf] ${NC} Avvio installazione ThiReMa ..."
 if [ -d "thirema" ] 
 then
+	echo "${ORANGE}[Mr. Wolf] ${NC} Rimozione dei servizi attivi ..."
 	docker-compose  \
 	-f thirema/docker-compose.yml \
 	-f thirema/docker-compose.thirema.yml \
 	down -v
+	docker image prune -f
 	rm -rf ./thirema/
+	echo "${ORANGE}[Mr. Wolf] ${GREEN} Rimozione eseguita!"
 fi
+echo "${ORANGE}[Mr. Wolf] ${NC} Scaricamento delle componenti software ..."
 mkdir thirema
 git clone https://github.com/RedRoundRobin/swe-kafka-db thirema/swe-kafka-db
 git clone https://github.com/RedRoundRobin/swe-gateway thirema/swe-gateway
 git clone https://github.com/RedRoundRobin/swe-api thirema/swe-api
 git clone https://github.com/RedRoundRobin/swe-telegram thirema/swe-telegram
 git clone https://github.com/RedRoundRobin/swe-webapp thirema/swe-webapp
-echo "> Have you ever given a foot massage? Stay calm and relax ..."
+echo "${ORANGE}[Mr. Wolf] ${GREEN} Componenti scaricate con successo!"
 cp ./docker-compose.thirema.yml thirema/
 mv ./thirema/swe-kafka-db/kafka/* thirema/
+echo "${ORANGE}[Mr. Wolf] ${NC} Installazione e avvio dei container Docker in corso..."
 docker-compose \
 	-f thirema/docker-compose.yml \
 	-f thirema/docker-compose.thirema.yml \
 	up --build -d
-echo "> Mr. wolf solved our problems."
+echo "${ORANGE}[Mr. Wolf] ${GREEN} Avvio dei container completato!"
+echo "============================================="
+echo "> ${ORANGE}Mr. Wolf ${GREEN} solved our ${RED} problems <"

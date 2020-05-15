081bede3f76d820bc2f3d9ff4cb3770ef82fb2c9
Mariano <marianosciacco@gmail.com>
2020-05-15 10:41:44 +0200

    Create downloader.sh

---
+++ install/production/downloader.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/RedRoundRobin/swe-kafka-db kafka-db &&
+git clone https://github.com/RedRoundRobin/swe-gateway gateway &&
+git clone https://github.com/RedRoundRobin/swe-api api &&
+git clone https://github.com/RedRoundRobin/swe-telegram telegram &&
+git clone https://github.com/RedRoundRobin/swe-webapp webapp &&
+mv ./riot/kafka-db/kafka/* ./ 
